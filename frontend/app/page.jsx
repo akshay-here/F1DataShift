d178c9e9e53bbe995963dea9e6b5488ffb1f4e7f
akshay-here <akshay.as2003@gmail.com>
2025-06-07 13:11:08 +0530

    init nextjs project

---
+++ frontend/app/page.jsx
@@ -0,0 +1,9 @@
+// Home page
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  );
+}
---
+++ frontend/app/standings/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Standings() {
+  return (
+    <div>
+        <h1>Standings Page</h1>
+    </div>
+  )
+}
+
+export default Standings

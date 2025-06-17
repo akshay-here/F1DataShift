c73e7c0c5b4f990167aae1d9b08fa6dcd4bd5950
akshay-here <akshay.as2003@gmail.com>
2025-06-18 00:31:14 +0530

    preparing to deploy the backend

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Frontend URL
+    allow_origins=["http://localhost:3000", "https://f1datashift-frontend.vercel.app"],  # Frontend URL
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -26,3 +26,6 @@ app.include_router(heatmap.router, prefix="/heatmap")
 app.include_router(racePlots.router, prefix="/raceplots")
 app.include_router(driverPlots.router, prefix="/driverplots")
 
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
